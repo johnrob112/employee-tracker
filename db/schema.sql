2388abe24ac6dfbb38cf4ac5f2368d06171bc8ab
John Robinson <jxr1372@miami.edu>
2022-11-14 15:26:11 -0500

    added seeds and server.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,37 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+
+
 CREATE TABLE department(
-id INT ,
-Department_name VARCHAR(30),
+id INTEGER AUTO_INCREMENT,
+Department_name VARCHAR(30) NOT NULL,
 PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  salary INTEGER,
+  department_id INTEGER,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,27 @@ VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal');
+
+INSERT INTO role(id, Title, Department_id, Salary)
+ VALUES  
+ (1, 'Sales Lead', 1, 100000),
+ (2, 'Salesperson', 1, 80000),
+ (3, 'Lead Engineer', 2, 150000),
+ (4, 'Software Engineer', 2, 120000),
+ (5, 'Account Manager', 3, 160000),
+ (6, 'Accountant', 3, 125000),
+ (7, 'Legal Team Lead', 4, 250000),
+ (8, 'Lawyer', 4, 190000);
+
+
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+ VALUES  
+ ('John', 'Doe', 1, NULL),
+ ('Mike', 'Chan', 1, 1),
+ ('Ashley', 'Rodriguez', 2, 1),
+ ('Kevin', 'Tupik', 2, 1),
+ ('Kunal', 'Singh', 3, 2),
+ ('Malia', 'Brown', 3, 2),
+ ('Sarah', 'Lourd', 4, 2),
+ ('Tom', 'Allen', 4, 2),
+ ('Sam', 'Kash', 1, 3);
